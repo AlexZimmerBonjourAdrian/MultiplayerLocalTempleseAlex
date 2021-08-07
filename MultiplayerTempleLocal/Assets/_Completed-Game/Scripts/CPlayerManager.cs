4efc156b0ff56876515e7d20fdfdf51653b3d04a
Alex Zimmer <alexzimmer.zimmer1@gmail.com>
2021-08-07 14:28:18 -0300

    Multiplayer Local Key Setting

---
+++ MultiplayerTempleLocal/Assets/_Completed-Game/Scripts/CPlayerManager.cs
@@ -0,0 +1,135 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPlayerManager : MonoBehaviour, IChange
+{
+    /*
+    public int _PlayerNumber;
+    [SerializeField]private GameObject _TankPrefab;
+    public List<CPlayerController> _Play;
+    */
+    // Start is called before the first frame update
+    public List<PlayerController> _PlayerList = new List<PlayerController>();
+    private List<GameObject> _ListObject = new List<GameObject>();
+    [SerializeField] private GameObject[] _AssetManager;
+    private GameObject _obj;
+    private Transform _serchTransform;
+
+    public static CPlayerManager Inst
+    {
+        get
+        {
+            if(_inst == null)
+            {
+                GameObject obj = new GameObject("CPlayerManager");
+                return obj.AddComponent<CPlayerManager>();
+            }
+            return _inst;
+        }
+    }
+    private static CPlayerManager _inst;
+
+    private void Awake()
+    {
+        if(_inst != null && _inst != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this.gameObject);
+        _inst = this;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = _PlayerList.Count - 1; i >= 0; i--)
+        {
+         if(_PlayerList[i]== null)
+            {
+                _PlayerList.RemoveAt(i);
+            }
+        }
+        for(int i = _ListObject.Count -1; i >= 0;i--)
+        {
+            if(_ListObject[i]==null)
+            {
+                _ListObject.RemoveAt(i);
+            }
+        }
+        if(CKeyboard.KeysGame.PressButton(CKeyboard.KeysGame.Key1))
+        {
+            changedCharacter(1, 0);
+        }
+        else if(CKeyboard.KeysGame.PressButton(CKeyboard.KeysGame.Key2))
+        {
+            changedCharacter(2, 0);
+        }
+        else if(CKeyboard.KeysGame.PressButton(CKeyboard.KeysGame.Key3))
+        {
+            changedCharacter(3, 0);
+        }
+        
+    }
+
+    public void Spawn(Vector2 Pos)
+    {
+        
+        if (_PlayerList.Count <= 2)
+        {
+            
+            GameObject obj = (GameObject)Instantiate(_AssetManager[0], Pos, Quaternion.identity);
+            PlayerController newPlayer = obj.GetComponent<PlayerA>();
+           // newPlayer.AsignControll();
+            _PlayerList.Add(newPlayer);
+            //_ListObject.Add(obj);
+            _obj = obj;
+            
+        }
+    }
+    public void changedCharacter(int count,int NumPlayer)
+    {
+    
+        PlayerController newPlayer;
+        _ListObject.Add(_obj);
+        
+        //for (int i = _PlayerList.Count - 1; i <= 0; i--)
+        //{
+            
+            switch (count)
+            {
+                case 1:
+                Destroy(_obj);
+                    _obj = (GameObject)Instantiate(_AssetManager[0], _ListObject[0].transform.position, Quaternion.identity);
+                    newPlayer = _obj.GetComponent<PlayerA>();
+                    _PlayerList[NumPlayer] = newPlayer;
+                    _ListObject[0] = null;
+                    
+                    /*
+                     _obj=(GameObject)Instantiate(_AssetManager[0],position)
+                    _PlayerList[i]=
+                    */
+                    break;
+                case 2:
+                Destroy(_obj);
+                _obj = (GameObject)Instantiate(_AssetManager[1], _ListObject[0].transform.position, Quaternion.identity);
+                    newPlayer = _obj.GetComponent<PlayerB>();
+                    _PlayerList[NumPlayer] = newPlayer;
+                    _ListObject[0] = null;
+                    break;
+                case 3:
+                Destroy(_obj);
+                _obj = (GameObject)Instantiate(_AssetManager[2], _ListObject[0].transform.position, Quaternion.identity);
+                    newPlayer = _obj.GetComponent<PlayerC>();
+                    _PlayerList[NumPlayer] = newPlayer;
+                    _ListObject[0] = null;
+                    break;
+            //}
+        }
+      
+    }
+   
+    
+}
---
+++ MultiplayerTempleLocal/Assets/_Completed-Game/Scripts/PlayerController.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+
+// Include the namespace required to use Unity UI
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class PlayerController : MonoBehaviour, IChange {
+	
+	// Create public variables for player speed, and for the Text UI game objects
+	[SerializeField] protected  float speed;
+	protected Text countText;
+	protected Text winText;
+
+	// Create private references to the rigidbody component on the player, and the count of pick up objects picked up so far
+	protected Rigidbody rb;
+	public int _PlayerCount=0;
+	protected int count;
+	protected int Control;
+	// At the start of the game..
+	public virtual void Start ()
+	{
+		// Assign the Rigidbody component to our private rb variable
+		rb = GetComponent<Rigidbody>();
+
+		// Set the count to zero 
+		count = 0;
+
+		// Run the SetCountText function to update the UI (see below)
+		//SetCountText ();
+
+		// Set the text property of our Win Text UI to an empty string, making the 'You Win' (game over message) blank
+		//winText.text = "";
+	}
+	
+	// Each physics step..
+	public virtual void FixedUpdate ()
+	{
+		// Set some local float variables equal to the value of our Horizontal and Vertical Inputs
+
+		Move();
+		// Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
+		
+
+		// Add a physical force to our Player rigidbody using our 'movement' Vector3 above, 
+		// multiplying it by 'speed' - our public player speed that appears in the inspector
+	
+	}
+	
+	protected virtual void Move()
+	{
+		float moveHorizontal;
+		float moveVertical;
+		Vector3 movement;
+		/*
+		switch (_PlayerCount)
+		{
+			case 1:
+				moveHorizontal = Input.GetAxis("Horizontal");
+				moveVertical = Input.GetAxis("Vertical");
+				movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+				rb.AddForce(movement * speed);
+				break;
+			case 2:
+				moveHorizontal= Input.GetAxis("HorizontalPlayer2");
+				moveVertical=Input.GetAxis("VerticalPlayer2");
+				movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+				rb.AddForce(movement * speed);
+				break;
+			default:
+				Debug.LogError("El control no se asigno");
+				break;
+		}
+			
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		*/
+		moveHorizontal = Input.GetAxis("Horizontal");
+		moveVertical = Input.GetAxis("Vertical");
+		movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce(movement * speed);
+
+
+	}
+	// When this game object intersects a collider with 'is trigger' checked, 
+	// store a reference to that collider in a variable named 'other'..
+	void  OnTriggerEnter(Collider other) 
+	{
+		// ..and if the game object we intersect has the tag 'Pick Up' assigned to it..
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			// Make the other game object (the pick up) inactive, to make it disappear
+			other.gameObject.SetActive (false);
+
+			// Add one to the score variable 'count'
+			count = count + 1;
+
+			// Run the 'SetCountText()' function (see below)
+			//SetCountText ();
+		}
+	}
+
+	// Create a standalone function that can update the 'countText' UI and check if the required amount to win has been achieved
+	/*
+	public virtual void SetCountText()
+	{
+		// Update the text field of our 'countText' variable
+		countText.text = "Count: " + count.ToString ();
+
+		// Check if our 'count' is equal to or exceeded 12
+		if (count >= 12) 
+		{
+			// Set the text value of our 'winText'
+			winText.text = "You Win!";
+		}
+	}
+	*/
+	public virtual void AsignControll()
+	{
+		_PlayerCount += 1;
+	}
+
+	
+}
