8b70252b85bf27b2f99ae9cb780975f6d356f8d6
Alex Zimmer <alexzimmer.zimmer1@gmail.com>
2021-12-04 21:11:39 -0300

    Update: Finish placeholder multicharacter system automatiali

---
+++ MultiplayerTempleLocal/Assets/_Completed-Game/Scripts/Entities/Pllayer/PlayerC.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerC : PlayerController
+{
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+ 
+    protected override void Move()
+    {
+        base.Move();
+    }
+    public override void AsignControll()
+    {
+        base.AsignControll();
+    }
+  
+}
